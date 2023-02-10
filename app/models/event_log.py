5058ec4be4b7b35088f94d570c21b2d35a2d1219
sakaijunsoccer <sakaijunsoccer@gmail.com>
2023-02-09 18:34:41 -1000

    Set default timeout 5 sec

--- app/models/event_log.py
+++ app/models/event_log.py
@@ -6,7 +6,7 @@ from typing import Union
 from utils.process import ThreadWithReturnValue
 
 DEFAULT_FIND_EVENT_NUM = 10
-DEFAULT_FIND_TIMEOUT = 5 * 60
+DEFAULT_FIND_TIMEOUT = 5
 DEFAULT_BUFFER_SIZE = 16 * 1024
 
 
